08f2ac056e6870293da892fb19d566505376a685
Umar Sunusi Maitalata <maitalata@gmail.com>
2023-05-14 06:52:20 +0100

    first commit

---
+++ wpf1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace wpf1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (running)
+            {
+                //stop
+               // tbStatus.Text = "Stopped";
+              //  btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                //run
+               // tbStatus.Text = "Running";
+              //  btnToggleRun.Content = "Stop";
+            }
+
+            running = !running;
+
+
+        }
+    }
+}
